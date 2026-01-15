803467a421da99b27da042312bce273ec91315fc
Mina <boles.minan@gmail.com>
2026-01-15 14:35:19 +0100

    Add program to print a name

---
+++ 1 - print your name.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+
+
+void PrintName( string Name)
+{
+    std::cout << "Your Name is : "<<Name;
+}
+
+int main()
+{
+    PrintName("SomeOne");
+}
